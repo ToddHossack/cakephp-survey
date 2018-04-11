510586d24f6760e5b5f827ed2c47589721a70beb
Andrey Vystavkin <jujav4ik@gmail.com>
2018-04-11 16:01:47 +0300

    Adding some more unit tests on surveys preview (task #5751)

--- tests/TestCase/Model/Table/SurveyResultsTableTest.php
+++ tests/TestCase/Model/Table/SurveyResultsTableTest.php
@@ -56,32 +56,48 @@ class SurveyResultsTableTest extends TestCase
     }
 
     /**
-     * Test initialize method
-     *
-     * @return void
+     * @dataProvider testGetResultsProvider
      */
-    public function testInitialize()
+    public function testGetResults($data, $expected)
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $user = ['id' => '123'];
+        $survey = (object)[
+            'id' => '111',
+        ];
+
+        $result = $this->SurveyResults->getResults($data, [
+            'user' => $user,
+            'survey' => $survey
+        ]);
+
+        $this->assertEquals(count($result), $expected);
     }
 
-    /**
-     * Test validationDefault method
-     *
-     * @return void
-     */
-    public function testValidationDefault()
+    public function testGetResultsProvider()
     {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test buildRules method
-     *
-     * @return void
-     */
-    public function testBuildRules()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
+        return [
+            [
+                [],
+                0
+            ],
+            [
+                [
+                    'user_id' => '123',
+                    'survey_question_id' => '123123',
+                    'survey_answer_id' => '123',
+                    'survey_id' => '321'
+                ],
+                1
+            ],
+            [
+                 [
+                    'user_id' => '123',
+                    'survey_question_id' => '123123',
+                    'survey_answer_id' => ['123', '456'],
+                    'survey_id' => '321'
+                 ],
+                 2
+            ]
+        ];
     }
 }
