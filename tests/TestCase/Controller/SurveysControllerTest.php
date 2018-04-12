9d6a281c66b443ba6fc449049b92038f4c32c5b1
Andrey Vystavkin <jujav4ik@gmail.com>
2018-04-12 11:19:08 +0300

    More integration tests for surveys controller (task #5751)

--- tests/TestCase/Controller/SurveysControllerTest.php
+++ tests/TestCase/Controller/SurveysControllerTest.php
@@ -28,6 +28,32 @@ class SurveysControllerTest extends IntegrationTestCase
         ]);
     }
 
+    public function testIndex()
+    {
+        $this->get('/surveys/surveys');
+        $this->assertResponseOk();
+    }
+
+    public function testAdd()
+    {
+        $this->get('/surveys/surveys/add');
+        $this->assertResponseOk();
+    }
+
+    public function testEdit()
+    {
+        $surveyId = '00000000-0000-0000-0000-000000000001';
+        $this->get('/surveys/surveys/edit/' . $surveyId);
+        $this->assertResponseOk();
+    }
+
+    public function testDeleteRedirectOk()
+    {
+        $surveyId = '00000000-0000-0000-0000-000000000001';
+        $this->delete('/surveys/surveys/delete/' . $surveyId);
+        $this->assertRedirect(['controller' => 'Surveys', 'action' => 'index']);
+    }
+
     public function testPreviewGet()
     {
         $surveyId = '00000000-0000-0000-0000-000000000001';
