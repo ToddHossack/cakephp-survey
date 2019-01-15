1225c7bef0cb70788af1e3751f926b9025327b85
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-01-15 17:15:16 +0200

    adding default surveys shell (Task #9558)

---
+++ src/Shell/SurveysShell.php
@@ -0,0 +1,36 @@
+<?php
+namespace Qobo\Survey\Shell;
+
+use Cake\Console\Shell;
+
+/**
+ * Surveys shell command.
+ */
+class SurveysShell extends Shell
+{
+    public $tasks = [
+        'Qobo/Survey.AddSections'
+    ];
+    /**
+     * Manage the available sub-commands along with their arguments and help
+     *
+     * @see http://book.cakephp.org/3.0/en/console-and-shells.html#configuring-options-and-generating-help
+     *
+     * @return \Cake\Console\ConsoleOptionParser
+     */
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+
+        $parser->setDescription('Surveys shell handles related survey tasks.')
+            ->addSubcommand(
+                'add_sections',
+                [
+                    'help' => 'Add default sections to existing surveys',
+                    'parser' => $this->AddSections->getOptionParser()
+                ]
+            );
+
+        return $parser;
+    }
+}
