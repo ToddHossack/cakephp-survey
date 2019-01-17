f496ce58f2f8b62b6b781fa1f97a43c95b98a053
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-01-17 02:42:12 +0200

    removing obsolete multiselect call (task #9558)

--- webroot/js/init.js
+++ webroot/js/init.js
@@ -9,8 +9,6 @@
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 (function ($) {
-    $('.my-select').multiSelect({ keepOrder: true });
-
     $('[data-provide="datetimepicker"]').each(function () {
         var options = {
             singleDatePicker: true,
