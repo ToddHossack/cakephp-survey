410a138231cbe3f796a5555ee5d450fea2ec0143
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-08-23 15:28:13 +0300

    adding migration script for surveys shell

--- src/Shell/SurveysShell.php
+++ src/Shell/SurveysShell.php
@@ -13,6 +13,7 @@ class SurveysShell extends Shell
     public $tasks = [
         'Qobo/Survey.AddDefaultSections',
         'Qobo/Survey.MoveSubmitsToEntries',
+        'Qobo/Survey.MigrateScoresToSurveyResults'
     ];
     /**
      * Manage the available sub-commands along with their arguments and help
@@ -37,6 +38,12 @@ class SurveysShell extends Shell
                 [
                     'help' => 'Pre-populating Survey Entry with all the submits that took place',
                     'parser' => $this->MoveSubmitsToEntries->getOptionParser()
+                ]
+            )->addSubcommand(
+                'migrate_scores_to_survey_results',
+                [
+                    'help' => 'Migrate score values to survey_results records',
+                    'parser' => $this->MigrateScoresToSurveyResults->getOptionParser()
                 ]
             );
 
